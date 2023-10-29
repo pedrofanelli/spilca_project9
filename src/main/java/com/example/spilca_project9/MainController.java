3051143a9a73b46ce1607f9484356be7a2329017
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-10-29 17:10:09 -0300

    sending attributes with THYMELEAF

--- src/main/java/com/example/spilca_project9/MainController.java
+++ src/main/java/com/example/spilca_project9/MainController.java
@@ -1,13 +1,16 @@
 package com.example.spilca_project9;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
 	@RequestMapping("/home")
-	public String home() {  // this is an ACTION
-		return "Home.html";
+	public String home(Model page) {  // this is an ACTION
+		page.addAttribute("username", "Katy");
+		page.addAttribute("color", "red");
+		return "Home2.html";
 	}
 }
