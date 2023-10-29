f637ce0a7714cdc59887efce7f06557f6798358d
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-10-29 17:31:03 -0300

    Request Parameters and Path Variables

--- src/main/java/com/example/spilca_project9/MainController.java
+++ src/main/java/com/example/spilca_project9/MainController.java
@@ -2,15 +2,21 @@ package com.example.spilca_project9;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-	@RequestMapping("/home")
-	public String home(Model page) {  // this is an ACTION
-		page.addAttribute("username", "Katy");
-		page.addAttribute("color", "red");
+	@RequestMapping("/home/{color}")
+	public String home(
+			//@RequestParam(required=false) String color, // using request parameters
+			//@RequestParam(required=false) String user,
+			@PathVariable String color,
+			Model page) {  // this is an ACTION
+		page.addAttribute("username", "Peter");
+		page.addAttribute("color", color);
 		return "Home2.html";
 	}
 }
