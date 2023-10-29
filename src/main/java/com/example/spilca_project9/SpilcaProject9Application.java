e39f2e756979d146c734b93cb3d8ffb3c718231c
Pedro Fanelli <pedrofanelli1@gmail.com>
2023-10-29 15:20:30 -0300

    start

---
+++ src/main/java/com/example/spilca_project9/SpilcaProject9Application.java
@@ -0,0 +1,13 @@
+package com.example.spilca_project9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpilcaProject9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpilcaProject9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spilca_project9/SpilcaProject9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spilca_project9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpilcaProject9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
